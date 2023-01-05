87e68d044ab7fd31fa266af2b7a9dfc773a23d0a
ononak <ononak@gmail.com>
2023-01-05 18:37:03 +0100

    event loop added

--- EventService.cpp
+++ EventService.cpp
@@ -1,12 +1,11 @@
 #include "EventService.hpp"
 #include "Event.hpp"
+#include <thread>
 #include <utility>
-
 
 std::shared_ptr<EventService> EventService::instance = nullptr;
 
 std::shared_ptr<EventService> EventService::get() {
-
   if (instance == nullptr) {
     instance = std::shared_ptr<EventService>(new EventService());
   }
@@ -14,14 +13,15 @@ std::shared_ptr<EventService> EventService::get() {
 }
 
 EventService::~EventService() {
-  std::cout << "EventService desctructor" << std::endl;
+  std::cout << "EventService destructor" << std::endl;
   notifier.disconnect_all_slots();
 }
 EventService::EventService() {}
 
 void EventService::publish(std::shared_ptr<Event> message) {
   std::lock_guard<std::mutex> lock(eventLoopMutex);
-  notifier(message);
+  std::thread eventLoopThread([&]() { notifier(message); });
+  eventLoopThread.join();
 }
 
 Token EventService::subscribe(const EventHandler &handler) {
